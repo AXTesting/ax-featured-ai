b33464b855863fc37eed49d0aa63a3098763737c
AXTesting <143734700+AXTesting@users.noreply.github.com>
2023-09-09 02:03:03 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -13,7 +13,49 @@ export default function Home() {
           <a href="/use/caption-genius">Caption Genius</a>
         </li>
         <li> 
-          <a href="/use/stock-image-assistant">Stock Image Assistant</a>
+          <a href="/use/the-learning-machine">The Learning Machine</a>
+        </li>
+        <li> 
+          <a href="/use/gift-ideas">Gift Ideas</a>
+        </li>
+        <li> 
+          <a href="/use/plumbing-expert">Plumbing Expert</a>
+        </li>
+         <li> 
+          <a href="/use/help-me-plan">Help Me Plan</a>
+        </li>
+        <li> 
+          <a href="/use/interview-questions-generator">Interview Questions Generator</a>
+        </li>
+        <li> 
+          <a href="/use/Brainstorm Content Ideas">brainstorm-content-ideas</a>
+        </li>
+        <li> 
+          <a href="/use/Ghostwriter">ghostwriter</a>
+        </li>
+        <li> 
+          <a href="/use/Engagement Strategies Assistant">engagement-strategies-assistant</a>
+        </li>
+        <li> 
+          <a href="/use/Personal Tutor">personal-tutor</a>
+        </li>
+        <li> 
+          <a href="/use/AI at first sight!">ai-at-first-sight</a>
+        </li>
+          <li> 
+          <a href="/use/LinkedIn Article Generator">linkedin-article-generator</a>
+        </li>
+        <li> 
+          <a href="/use/Meal Planner">meal-planner</a>
+        </li>
+        <li> 
+          <a href="/use/Personal Trainer">personal-trainer</a>
+        </li>
+        <li> 
+          <a href="/use/The Pareto Principle">the-pareto-principle</a>
+        </li>
+        <li> 
+          <a href="/use/Five Whys Technique">five-whys-technique</a>
         </li>
       </ul>
     </main>
