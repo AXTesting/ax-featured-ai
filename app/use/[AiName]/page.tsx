81bc7518ecc38d1d6afeb5f873225ace6846e63d
AXTesting <143734700+AXTesting@users.noreply.github.com>
2023-09-15 01:09:24 +0530

    Update page.tsx

--- app/use/[AiName]/page.tsx
+++ app/use/[AiName]/page.tsx
@@ -46,7 +46,7 @@ const UseAi = (props: any) => {
         <input
           autoFocus
           type="text"
-          placeholder="Insert your name to proceed"
+          placeholder="Enter your name to proceed"
           value={username}
           onChange={(e) => onSetUsername(e.target.value)}
         />
